c77affd54ba4019fefed91fe766e578487e4ad29
Stephanie Kovalchik <skovalch@rand.org>
2014-08-04 14:32:34 -0400

    forwardslash fix in top player url added to fetch_url

--- R/fetch_url.R
+++ R/fetch_url.R
@@ -16,15 +16,15 @@ fetch_url <- function(name) {
         sub("(.*)(Tennis/Players/.*aspx)(.*)", "\\2", result[index])
     } else if (length(query.terms)>1){
    
-    	url <- "/Tennis/Players/Last/First/Full-Name.aspx"
+    	url <- "Tennis/Players/Last/First/Full-Name.aspx"
     	F <- substr(query.terms[1],1,1)
     	L <- substr(query.terms[length(query.terms)],1,2)
     	url <- sub("First", F, url)
     	url <- sub("Last", L, url)
     	url <- sub("Full-Name",paste(query.terms,sep="",collapse="-"),url)
-    	test.url <- tryCatch(open(url(paste("http://www.atpworldtour.com",url,sep=""))), error = function(x) NA)
-    	if(is.na(test.url)){
-    		url <- "/Tennis/Players/Top-Players/Full-Name.aspx"	
+    	test.url <- tryCatch(open(url(collapse("http://www.atpworldtour.com/",url))), error = function(x) NA)
+    	if(!is.null(test.url)){
+    		url <- "Tennis/Players/Top-Players/Full-Name.aspx"	
     		url <- sub("Full-Name",paste(query.terms,sep="",collapse="-"),url)
     	}    		
       url
