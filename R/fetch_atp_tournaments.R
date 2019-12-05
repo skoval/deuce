a0b96ac7f06ad6ad167ace692666d4c4b2b80d85
skoval <s.a.kovalchik@gmail.com>
2019-12-05 11:51:28 +1100

    Rewrote fetch_atp_tournaments to use a rvest implementation

--- R/fetch_atp_tournaments.R
+++ R/fetch_atp_tournaments.R
@@ -6,106 +6,51 @@
 #'
 #' @export
 fetch_atp_tournaments <- function(challenger = FALSE){
+
+	if(challenger)
+		url <- "http://www.atpworldtour.com/en/atp-challenger-tour/calendar"
+	else
+		url <- "http://www.atpworldtour.com/en/tournaments"
+		
+	page <- read_html(url)
 	
-	if(challenger){
-		tournaments <- readLines("http://www.atpworldtour.com/en/atp-challenger-tour/calendar")
-		tourneys <- grep("tourney-title", tournaments)
-	}
-	else{
-		tournaments <- readLines("http://www.atpworldtour.com/en/tournaments")
-		tourneys <- grep("tourney-title", tournaments)
-	}
-		tournament_list <- mapply(function(x, y){tournaments[x:y]}, x= tourneys, y =c(tourneys[-1], max(tourneys) + 100) )
-
-	extract_fields <- function(lines){
-
-		item_values <- grep("item-value", lines) 
-		tournament_name <- grep("[A-Z]", lines)[1] # First occurrence
-		tournament_name <- sub("(.*title.*>)(.*)(</a.*)", "\\2", lines[tournament_name])
-		tournament_name <- gsub("\t", "", tournament_name)
-
-		date <-  grep("([0-9][0-9][0-9][0-9]\\.[0-9][0-9]\\.[0-9][0-9])", lines)[1] # First dates
+	tournaments <- page %>% html_nodes(".tourney-title") %>%
+		html_attr("data-ga-label")
 		
-		location <-  gsub("\t", "", lines[grep("tourney-location", lines)[1] + 1])
+	locations <- page %>% html_nodes(".tourney-location") %>%
+		html_text(trim = T)
 
-		draw <- grep("SGL", lines)[1]
-		draw <- item_values[item_values > draw][1] + 1
-
-		surface1 <- grep("door", lines)[1] 
-		surface2 <- grep("(Hard|Grass|Clay)", lines)[1]
+	dates <- page %>% html_nodes(".tourney-dates") %>%
+		html_text(trim = T)
 		
-		prize <- grep("Financial", lines)[1]
-		prize <- item_values[item_values > prize][1] + 1
-		prize <- gsub(",", "", lines[prize])
-
-
-		prize_extract <- function(x){
-			numbers <- str_extract_all(x, "[0-9]")
-			numbers <- collapse(numbers[[1]])
-			location <- str_locate(x, "[0-9]")[1]
-			currency <- substr(x, location - 1, location - 1)
-			if(currency == "#") currency <- "£"
-			if(grepl("A\\$", x)) currency <- "A$"
-		collapse(currency, numbers)
-		}
+	start_date <- str_extract(dates, "^[0-9][0-9][0-9][0-9].[0-9][0-9].[0-9][0-9]")
+	end_date <- str_extract(dates, "[0-9][0-9][0-9][0-9].[0-9][0-9].[0-9][0-9]$")
+	
+	details <- page %>% html_nodes(".tourney-result") 
+	
+	item_details <- lapply(details, function(x){
+		x %>%
+		html_nodes(".tourney-details") %>%
+		html_nodes(".item-value") %>%
+		html_text(trim = T)		
+		})
 		
-		if(grepl("[0-9]", prize))
-			prize <- prize_extract(prize)
-		else
-			prize <- NA
-
-		start_date <- sub("([0-9][0-9][0-9][0-9]\\.[0-9][0-9]\\.[0-9][0-9])(.*)","\\1", lines[date])
-		end_date <- sub("([0-9][0-9][0-9][0-9]\\.[0-9][0-9]\\.[0-9][0-9].*)(.*)([0-9][0-9][0-9][0-9]\\.[0-9][0-9]\\.[0-9][0-9])(.*)","\\3", lines[date])
-		draw_size <-  gsub("\t","", lines[draw])
-
-		if(!is.na(surface1)){
-			surface_type <- ifelse(grepl("Outdoor", lines[surface1]), "Outdoor", "Indoor")
-			if(!is.na(surface2)){
-				surface_type2 <- ifelse(grepl("Hard", lines[surface2]), "Hard",
-						ifelse(grepl("Clay", lines[surface2]), "Clay", "Grass"))
-				surface_type <- paste(surface_type, surface_type2)
-			}
-		}
-		else{
-			if(!is.na(surface2)){
-				surface_type <- ifelse(grepl("Hard", lines[surface2]), "Hard",
-						ifelse(grepl("Clay", lines[surface2]), "Clay", "Grass"))
-			}
-			surface_type <- NA
-		}
-
-
+	draw_size <- as.numeric(sapply(item_details, function(x) x[1])) # Take first number
+	
+	surface <- sapply(item_details, function(x) ifelse(any(x == "Clay"), "Clay", ifelse(any(x == "Grass"), "Grass", "Hard")))
+	
+	prize <- sapply(item_details, function(x) ifelse(grepl("[0-9]", x[length(x)]), x[length(x)], NA))
+		
 	data.frame(
-		name = tournament_name,
-		location = location,
+		name = tournaments,
+		location = locations,
 		start_date = as.Date(start_date, format = "%Y.%m.%d"),
 		end_date = as.Date(end_date, format = "%Y.%m.%d"),	
 		draw = as.numeric(draw_size),
 		matches = as.numeric(draw_size) - 1,
-		surface = surface_type,
-		prize = prize
-	)
-	}
+		surface = surface,
+		prize = prize,
+		stringsAsFactors = F
+	)		
 
-	tier <- grep("categorystamps", tournaments)
-	
-	ntournaments <- length(tournament_list)
-	
-	tier <- tier[(length(tier)-ntournaments+1):length(tier)]
-	
-	tier_name <- sub("(.*categorystamps_)(.*)(\\..*)", "\\2", tournaments[tier])		
-	
-	tier <- ifelse(tier_name == "1000s", "1000",
-				ifelse(tier_name == "finals-pos", "Tour Finals",
-					ifelse(tier_name == "grandslam", "Grand Slam", tier_name)))
-
-
-	
-	obj <- do.call("rbind", lapply(tournament_list, extract_fields))
-
-	if(challenger)
-		obj$tier <- "challenger"
-	else
-		obj$tier <- tier
-obj	
 }
